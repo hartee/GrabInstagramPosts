07d09befae69422cd41079a9ddfd2919a0486eb5
eah <erikanthony.harte@gmail.com>
2021-01-24 14:46:26 -0800

    initial commit (sort of)

---
+++ grab.zsh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+cd /Users/ea/Projects/NewAdventure/GrabInstagram/grabInstagram
+/usr/local/bin/pipenv run python main.py
+cp /Users/ea/Projects/NewAdventure/GrabInstagram/grabInstagram/*.md /Users/ea/Projects/Portfolio/Portfolio/content/music/2020
+cd /Users/ea/Projects/Portfolio/Portfolio/
+/usr/local/bin/hugo
+/usr/local/bin/git status
+/usr/local/bin/git add .
+/usr/local/bin/git commit -m "Autogenerate new music posts"
+/usr/local/bin/git push
