432e72a4ea660a770b9eac8549e922463701e33d
eah <erikanthony.harte@gmail.com>
2021-01-26 16:55:06 -0800

    change name of main python script

--- grab.zsh
+++ grab.zsh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 cd /Users/ea/Projects/NewAdventure/GrabInstagram/grabInstagram
-/usr/local/bin/pipenv run python main.py
+/usr/local/bin/pipenv run python grabMusic.py
 cp /Users/ea/Projects/NewAdventure/GrabInstagram/grabInstagram/*.md /Users/ea/Projects/Portfolio/Portfolio/content/music/2020
 cd /Users/ea/Projects/Portfolio/Portfolio/
 /usr/local/bin/hugo
